d2cdc4c8b59d538c3525d0f369b9449830b68437
zahid <zahidhanif208@gmail.com>
2022-04-20 04:23:06 +0500

    fix

--- db/migrate/20220418191731_add_missing_attr.rb
+++ db/migrate/20220418191731_add_missing_attr.rb
@@ -8,24 +8,12 @@ class AddMissingAttr < ActiveRecord::Migration[6.1]
     add_column :appointments, :client_id, :integer
     add_column :services, :service_id, :integer
 
-    add_column :services, :day, :string
-    add_column :services, :start_time, :time
-    add_column :services, :end_time, :time
-    add_column :services, :service_id, :integer
-    add_column :services, :service_provider_id, :integer
+    add_column :service_availabilities, :day, :string
+    add_column :service_availabilities, :start_time, :time
+    add_column :service_availabilities, :end_time, :time
+    add_column :service_availabilities, :service_id, :integer
+    add_column :service_availabilities, :service_provider_id, :integer
 
   end
 end
 
-# class UserTimeSlot < ApplicationRecord
-#   belongs_to :time_slot
-#   belongs_to :service_provider, :class_name => "User"
-#   belongs_to :appointment
-# end
-
-# class Appointment < ApplicationRecord
-#   has_many   :user_time_slots
-#   belongs_to :client, :class_name => "User"
-#   belongs_to :service_provider, :class_name => "User"
-#   belongs_to :service
-# end
